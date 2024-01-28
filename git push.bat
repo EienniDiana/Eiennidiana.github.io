bf007158041a714ba77656c9f39a557864633c75
eiennidiana <eiennidiana@outlook.com>
2024-01-29 00:13:13 +0800

    push

--- git push.bat
+++ git push.bat
@@ -1,5 +1,8 @@
 @echo on
 git add .
 git commit -m "push"
+git remote add origin https://github.com/eiennidiana/eiennidiana.github.io.git
+git fetch origin hexosource
+git checkout -b hexosource
 git push origin hexosource
 pause
